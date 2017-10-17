a7d1679449e039a4a7521602bfc77ed9e064fd4b
Trevor Sisk <trevorsisk228@gmail.com>
2017-10-17 02:09:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,6 +21,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars,foo){
 }
 
 function accessElementInArray(chocolateBars, index){
-  index = 1;
+  var index = 1;
   return chocolateBars[index];
 }
