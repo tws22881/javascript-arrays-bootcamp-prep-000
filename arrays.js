0981a9fdba7dcce38e91fafee78c16b90a4c9499
Trevor Sisk <trevorsisk228@gmail.com>
2017-10-17 02:15:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function accessElementInArray(chocolateBars, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
-  chocolateBars.slice(0);
+  chocolateBars.unshift(0);
   return chocolateBars;
 
 }
