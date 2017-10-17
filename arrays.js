92cf444db99aec1aa277444b8b8b8c1bc7f326b4
Trevor Sisk <trevorsisk228@gmail.com>
2017-10-17 01:57:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,8 +2,10 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(chocolateBars,foo){
   chocolateBars.unshift(foo);
+  return chocolateBars;
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars,foo){
   var chocolateBars = [foo, ...chocolateBars];
+  return chocolateBars;
 }
